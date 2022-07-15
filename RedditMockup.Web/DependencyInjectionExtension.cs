7aa1e82968fec05faa411823e9147f2881c36dba
Sepehr Frd <sepfrd@hotmail.com>
2022-07-15 23:44:48 +0430

    Implement RedditMockup.Web project

---
+++ RedditMockup.Web/DependencyInjectionExtension.cs
@@ -0,0 +1,108 @@
+﻿using System.IO.Compression;
+using System.Text.Json.Serialization;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using NLog.Web;
+using RedditMockup.Api.Contracts;
+using RedditMockup.Api.Filters;
+using RedditMockup.Business.Contracts;
+using RedditMockup.Common.Contracts;
+using RedditMockup.Common.Profiles;
+using RedditMockup.Common.Validations;
+using RedditMockup.DataAccess;
+using RedditMockup.DataAccess.Context;
+using RedditMockup.DataAccess.Contracts;
+using RedditMockup.Model.Entities;
+using Sieve.Services;
+
+namespace RedditMockup.Web;
+
+internal static class DependencyInjectionExtension
+{
+    internal static IServiceCollection InjectApi(this IServiceCollection services) =>
+        services
+            .AddControllers()
+            //.AddControllers(x => x.Filters.Add<CustomExceptionFilter>())
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+            })
+            .AddApplicationPart(typeof(IBaseController<BaseEntity, IBaseDto>).Assembly)
+            .Services
+            .AddHealthChecks()
+            .Services;
+
+    internal static IServiceCollection InjectSwagger(this IServiceCollection services) =>
+        services.AddSwaggerGen();
+
+    internal static IServiceCollection InjectUnitOfWork(this IServiceCollection services) =>
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+    internal static IServiceCollection InjectContext(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        return services.AddDbContext<RedditMockupContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("Default"), x => x.MigrationsAssembly("RedditMockup.DataAccess")));
+
+    }
+
+    internal static IServiceCollection InjectNLog(this IServiceCollection services,
+        IWebHostEnvironment environment)
+    {
+        var factory = NLogBuilder.ConfigureNLog(
+                environment.IsProduction()
+                    ? "nlog.config"
+                    : $"nlog.{environment.EnvironmentName}.config");
+        return services.AddSingleton(_ => factory.GetLogger("Info"))
+            .AddSingleton(_ => factory.GetLogger("Error"));
+    }
+
+    internal static IServiceCollection InjectSieve(this IServiceCollection services) =>
+        services.AddScoped<ISieveProcessor, SieveProcessor>();
+
+    internal static IServiceCollection InjectAuthentication(this IServiceCollection services) =>
+        services
+            .AddAuthorization()
+            .AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = options.DefaultChallengeScheme =
+                    CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddCookie(options =>
+            {
+                options.LoginPath = "/Account/Login";
+                options.LogoutPath = "/Account/Logout";
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+                options.AccessDeniedPath = "/Account/AccessDenied";
+            })
+            .Services;
+
+    internal static IServiceCollection InjectBusinesses(this IServiceCollection services) =>
+        services.Scan(scan =>
+                scan.FromAssembliesOf(typeof(IBaseBusiness<>))
+                    .AddClasses(classes =>
+                        classes.AssignableTo(typeof(IBaseBusiness<>)))
+                    .AsImplementedInterfaces()
+                    .WithScopedLifetime()
+                    .AddClasses(classes =>
+                        classes.Where(predicate =>
+                            predicate.Name.EndsWith("Business") && !predicate.IsAssignableTo(typeof(IBaseBusiness<>))))
+                    .AsSelf()
+                    .WithScopedLifetime());
+
+    internal static IServiceCollection InjectContentCompression(this IServiceCollection services) =>
+        services.Configure<GzipCompressionProviderOptions>
+                (options => options.Level = CompressionLevel.Fastest)
+            .AddResponseCompression(options => options.Providers.Add<GzipCompressionProvider>());
+
+    internal static IServiceCollection InjectFluentValidation(this IServiceCollection services) =>
+        services.AddFluentValidation(fv =>
+            fv.RegisterValidatorsFromAssemblyContaining<RoleValidator>());
+
+    internal static IServiceCollection InjectAutoMapper(this IServiceCollection services) =>
+        services.AddAutoMapper(typeof(UserProfile).Assembly);
+}
---
+++ RedditMockup.Web/Program.cs
@@ -0,0 +1,80 @@
+﻿using NLog.Web;
+using RedditMockup.DataAccess.Context;
+using RedditMockup.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Host.ConfigureAppConfiguration(config =>
+    config.SetBasePath(Directory.GetCurrentDirectory()).AddEnvironmentVariables());
+
+builder.Host.ConfigureLogging(x => x.ClearProviders().SetMinimumLevel(LogLevel.Trace));
+
+builder.Host.UseNLog();
+
+var logger = NLogBuilder.ConfigureNLog(
+        builder.Environment.IsProduction()
+            ? "nlog.config"
+            : $"nlog.{builder.Environment.EnvironmentName}.config")
+    .GetLogger("Info");
+
+try
+{
+    builder.Services
+        .InjectApi()
+        .InjectSwagger()
+        .InjectUnitOfWork()
+        .InjectSieve()
+        .InjectAuthentication()
+        .AddEndpointsApiExplorer()
+        .InjectNLog(builder.Environment)
+        .InjectContext(builder.Configuration)
+        .InjectBusinesses()
+        .InjectFluentValidation()
+        .InjectAutoMapper()
+        .InjectContentCompression();
+
+
+    var app = builder.Build();
+
+    await using var scope = app.Services.CreateAsyncScope();
+
+    await using var context = scope.ServiceProvider.GetRequiredService<RedditMockupContext>();
+
+    await context.Database.EnsureCreatedAsync();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    else
+    {
+        app.UseExceptionHandler("/Error")
+            .UseHsts();
+    }
+
+    app.UseHttpsRedirection()
+        .UseStaticFiles()
+        .UseRouting()
+        .UseAuthentication()
+        .UseAuthorization()
+        .UseEndpoints(endpoints =>
+    {
+        if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Testing"))
+            endpoints.MapHealthChecks("HealthCheck");
+        endpoints.MapControllers();
+    });
+
+    await app.RunAsync();
+}
+catch (Exception exception)
+{
+    logger.Error(exception, "Program Stopped Because of Exception !");
+    throw;
+}
+finally
+{
+    NLog.LogManager.Shutdown();
+}
